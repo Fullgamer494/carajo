d8593d5c06c33f7c78011189fff0cb41e454129c
Ilyavosky <ilyacortesruiz@gmail.com>
2025-11-12 20:22:51 -0600

    Añadir el plugin de status-pages

--- back/src/main/kotlin/Application.kt
+++ back/src/main/kotlin/Application.kt
@@ -14,6 +14,7 @@ fun Application.module() {
     configureSerialization()
     configureHTTP()
     configureMonitoring()
+    configureStatusPages()
     configureSecurity()
     configureRouting()
 }
